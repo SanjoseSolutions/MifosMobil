a2429cdbac6abcc82eb3fb609267e7d00705b4c0
Tshesko1 <francis@sanjosesolutions.in>
2016-02-12 17:35:07 +0000

    PopUp Confirm Message When Logout button clicked

--- www/js/app.js
+++ www/js/app.js
@@ -23,6 +23,30 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 })
 
+/*
+
+ .controller('NameOfTheCtrl',function($scope, $ionicPopup) { //, $timeout) {  
+   // A confirm dialog 'PopupCtrl'
+
+   $scope.ConfirmLogOut = function() {
+     var confirmPopup = $ionicPopup.confirm({
+       title: 'Confirm Logout',
+       template: '<strong>No Data Access</strong> if you Logout.\n Do You confirm ?'
+     });
+     confirmPopup.then(function(res) {
+       if(res) {
+        // "logout()" Can be Called here
+         console.log('Logout Confirmed !');
+       } else {
+         console.log('Logout Cancelled !');
+       }
+     });
+   };
+});
+
+*/
+
+
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
