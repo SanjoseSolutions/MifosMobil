063ca4ebbb7c9fe5ad11cc04107758d5f6989317
Terence Denzil Monteiro <terence@sanjosesolutions.in>
2016-05-12 18:08:50 +0530

    Added more node scripts

---
+++ scripts/get-client.js
@@ -0,0 +1,25 @@
+var request = require("request");
+var baseUrl = "https://kifiya.openmf.org/fineract-provider/api/v1";
+var headers = {
+    'Content-Type': 'application/json',
+    'Fineract-Platform-TenantId': 'kifiya',
+    'Authorization': 'Basic bWlmb3M6a2Z0ZWNoMTIz'
+};
+
+var clientId = "4"; // ToDo: remove hard coding
+
+request( {
+  url: baseUrl + "/staff" + clientId,
+  headers: headers,
+}, function(error, response, body) {
+  if (error) {
+    console.log("Error: " + error);
+  } else {
+    console.log("Success!");
+    if (!(body instanceof Object)) {
+      body = JSON.parse(body);
+    }
+    console.log(JSON.stringify(body, null, 4));
+  }
+} );
+
